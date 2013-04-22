9a3993307ed24e61ccd35ea3d4fa50833e826f1a
ethesx <ethesx@gmail.com>
2013-04-21 21:23:11 -0400

    Added db access, csv populaion

--- server.js
+++ server.js
@@ -8,6 +8,28 @@ var app = express();
 var records = [];
 
 
+csv(records)
+   //.from.stream(fs.createReadStream(__dirname + '/contract.txt'), {
+   .from.stream(fs.createReadStream(__dirname + '/diseaselinks.csv'), {
+   columns: true
+})
+   .on('record', function (row, index) {
+   records.push(row);
+
+   //console.log(row);
+})
+   .on('end', function (count) {
+   var MongoClient = require('mongodb').MongoClient;
+   // Connect to the db
+   MongoClient.connect("mongodb://localhost:27017/final", function (err, db) {
+      var collection = db.collection('disease')
+      collection.insert(records, function (err, doc) {
+         console.log(doc);
+      });
+   });
+   console.log('Number of lines: ' + count);
+});
+
 
 app.configure(function () {
    app.set('port', process.env.PORT || 2600);
