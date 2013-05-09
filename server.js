c3ed849010c516f8522e8cc6ec919a03a9f72557
ethesx <ethesx@gmail.com>
2013-05-08 22:22:52 -0400

    Setup mongodb singleton, global access

--- server.js
+++ server.js
@@ -2,13 +2,76 @@ var express = require('express'),
    fs = require('fs'),
    http = require('http'),
    csv = require('csv'),
-   path = require('path');
+   path = require('path'),
+   assert = require('assert');
    
 var app = express();
+
+
 
 var records = new Array();
 var records = [];
 var dataDir = "/data";
+
+
+var errors = {
+   
+   db : {missingparams : "Please specify all parameters to create a connection.",
+         typenotsupported : "The DB type is not supported.",
+         }
+   };
+
+//TODO move to js, use export, require. Check db closure, reopen
+// Connect to the db
+var DbSingleton = (function(){
+   
+      var instance = {};
+      var dbType = {
+         mongo : {name : "mongo", protocol : "mongodb"}
+         
+         };
+         
+      function privGetInstance(type, url, port, dbName){
+         
+         for(k in arguments){
+            if(!arguments[k] || arguments[k] === null){
+               console.error(errors.db.missingparams);
+               break;
+            }
+         }
+ 
+         if(type === dbType.mongo.name){
+            console.info("Trying " + dbType.mongo.name);
+            
+            var MongoClient = require('mongodb').MongoClient;
+            var MongoServer = require('mongodb').Server;
+            
+            var mclient = new MongoClient(new MongoServer(url, port, {auto_reconnect: true}))
+            mclient.open(function(){});
+            instance[dbType.mongo.name] = mclient.db(dbName);
+            return instance[dbType.mongo.name];
+         }
+         else{
+            console.error(errors.db.typenotsupported);
+            return null;
+         }
+      };
+      
+      
+      return{
+         
+         getInstance : function(type, url, port, db){
+                
+                if (!instance[type]){
+                  instance[type] = privGetInstance(type, url, port, db);
+                }
+
+                return instance[type];
+            }
+         };
+   
+   })();
+
 
 
 csv(records)
@@ -24,15 +87,10 @@ csv(records)
    //console.log(row);
 })
    .on('end', function (count) {
-   var MongoClient = require('mongodb').MongoClient;
-   // Connect to the db
-   MongoClient.connect("mongodb://localhost:27017/final", function (err, db) {
-      var collection = db.collection('disease_by_node')
-      collection.insert(records, function (err, doc) {
-         console.log(doc);
-      });
-   });
-   console.log('Number of lines: ' + count);
+      //collection.insert(records, function (err, doc) {
+               //console.log(doc);
+       //     });
+      console.log('Number of lines: ' + count);
 });
 
 
@@ -61,36 +119,16 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function(socket){
    socket.on('getData', function(){
          console.log("Sending default data *************************");
-         socket.emit('defaultData', ({ "disease" : "Lyme", "Syphilis" : "0", "Gonorrhea" : "0", "Chlamydia" : "0", "Chancroid" : "0", "Hepatitis B" : "4", "Hepatitis C" : "10", "H1N1 (Swine Flu)" : "50", "Lyme" : "0"}));
+         //socket.emit('defaultData', ({ "disease" : "Lyme", "Syphilis" : "0", "Gonorrhea" : "0", "Chlamydia" : "0", "Chancroid" : "0", "Hepatitis B" : "4", "Hepatitis C" : "10", "H1N1 (Swine Flu)" : "50", "Lyme" : "0"}));
    
    });
 });
 
-/*Migrate this sql to mongo
- *
- *SELECT disease, count(disease) cnt
-FROM disease_by_node
-WHERE PID IN
-  (
-    SELECT PID
-    FROM disease_by_node
-    WHERE disease = 'PERTUSSIS'
-  )
-AND disease <> 'PERTUSSIS'
-GROUP BY disease
-order by cnt desc;
-
-*/
 
 
-//Move to factory
-var mdb = require('mongodb').MongoClient;
-mdb.connect("mongodb://localhost:27017/final", function (err, db) {
-   var disease_by_node = db.collection('disease_by_node');
-   
-   var recs = disease_by_node.find({DISEASE : "PERTUSSIS"});
-   console.log("RETURNED RECORDS: **************************************" + recs);
-//iterate over rec results
-//db.disease_by_node.find({PID : "39481", DISEASE : {$ne: "PERTUSSIS"}}, {DISEASE:1});
-   });
-      
+//TODO Move arguments to object
+var db = DbSingleton.getInstance("mongo", "localhost", 27017, "final");
+var collection = db.collection('disease_by_node');
+
+//TODO work with result set. Manipulate to JSON
+collection.find({PID : "39481", DISEASE : {$ne: "PERTUSSIS"}}, {DISEASE:1});
